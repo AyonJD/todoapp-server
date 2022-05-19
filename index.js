c91509df7ee7e6cebdd12af4502d0c1ef87e3a37
Ayon <ayonjodder177@gmail.com>
2022-05-19 06:29:02 +0600

    Cors updated

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 const app = express();
